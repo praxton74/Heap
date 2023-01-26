d2657bde6d5f1e2f9873f2e7aee1d175cfcf72f2
Parth <94388365+praxton74@users.noreply.github.com>
2023-01-26 20:15:32 +0530

    Update and rename README.md to README.cpp

---
+++ README.cpp
@@ -0,0 +1,12 @@
+# Heap
+
+priority_queue<int> pq;
+  
+priority_queue <int, vector<int>, greater<int> > pq;
+
+  
+ A priority queue is a type of dynamic container in STL. It stores the elements in a sorted order where the first element of the queue is the greatest of all and the other elements are sorted in non-increasing order. 
+
+They are implemented as a heap in memory. The greatest element will be the one that will be deleted first. They are present in #include<queue> header file.
+  
+  
